1e3210a578567065f12e21ae4b37ceca97a13d9a
Jorge Romo <andresrt952@gmail.com>
2023-01-20 11:40:21 -0500

    reversa

--- ejercicio12.js
+++ ejercicio12.js
@@ -1,10 +1,2 @@
 //Crear una funcion que permita calcular el factorial de un numero entero
-let numero = 10;
-let factorial = 1;
 
-for (let index = 1; index <= numero; index++) {
-    factorial = factorial * index;
-  
-    
-}
-console.log(`El factorial de ${numero} es ${factorial}`);
