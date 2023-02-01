4b145f9d95c8b3d95e0c1f2c59ef5bfc967ff3a4
Jorge Romo <andresrt952@gmail.com>
2023-02-01 16:51:07 -0500

    martes 1 de febrero

--- ejercicio23.js
+++ ejercicio23.js
@@ -1,6 +1,19 @@
-//array simple
-let array = [1,2,3];
-//array multidimensional
-let arrayM = [[1,2,3],[4,5,6],[7,8,9]];
+//sumar las diagonales de un array de 3 dimensiones y dar como resultado la diferencia absoluta de las diagonales.
+let array = [[-5,2,3],
+             [4,5,6],
+             [9,8,9]];
+let suma1 = 0;
+let suma2 = 0;
 
-console.log(arrayM[2][1]);
+for (let i = 0; i < array.length; i++) {
+    suma1 = suma1 + array[i][i];
+    suma2 = suma2 + array[i][array.length - 1 -i];
+
+}
+
+console.log(suma1);
+console.log(suma2);
+
+let absoluto = Math.abs(suma1-suma2);
+
+console.log(`La resta absoluta de ${suma1} menos ${suma2} es ${absoluto}`);
